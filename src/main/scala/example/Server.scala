abf428b80b442c41bdf6c1222a1d1e85a3b2aaa3
Lasse Nørregaard <lassenorregaard@gmail.com>
2023-05-08 21:22:50 +0200

    Remove comments

--- src/main/scala/example/Server.scala
+++ src/main/scala/example/Server.scala
@@ -51,13 +51,6 @@ object Server extends ZIOAppDefault {
       }
       _ <- zio.Console.readLine
     } yield ()
-
-    //   val
-    //   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    //   StdIn.readLine() // let it run until user presses return
-    //   bindingFuture
-    //     .flatMap(_.unbind()) // trigger unbinding from the port
-    //     .onComplete(_ => system.terminate()) // and shutdown when done
   }
 
 }
